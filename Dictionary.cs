d35590115894c0d59fc24751182eaabd135bab9c
Tupikov Ivan <i.tupikov12@gmail.com>
2021-01-21 15:34:44 +0500

    Add function: CreateNewDictionary, AddWord

--- Dictionary.cs
+++ Dictionary.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace DictionaryApp
 {
@@ -13,6 +14,42 @@ namespace DictionaryApp
         public Dictionary()
         {
             fileName = "Dictionary.xml";
+        }
+
+        public void CreateNewDictionary(string DictionaryName)
+        {
+            XmlTextWriter writer = new XmlTextWriter($"{DictionaryName}.xml", Encoding.Unicode);
+            writer.Formatting = Formatting.Indented;
+
+            writer.WriteStartDocument();
+
+            writer.WriteStartElement($"{DictionaryName}");
+            
+            writer.WriteEndElement();
+
+            writer.Close();
+        }
+
+        public void AddWord(string DictionaryName, string word, string translation)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            
+            doc.Load($"{DictionaryName}.xml");
+
+            XmlNode root = doc.DocumentElement;
+
+            XmlNode newWord = doc.CreateElement(word);
+            XmlNode translate = doc.CreateElement("Translate");
+            XmlNode wordTranslation = doc.CreateTextNode(translation);
+
+            translate.AppendChild(wordTranslation);
+            newWord.AppendChild(translate);
+
+
+            root.AppendChild(newWord);
+
+            doc.Save($"{DictionaryName}.xml");
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace DictionaryApp
 {
@@ -10,7 +11,13 @@ namespace DictionaryApp
     {
         static void Main(string[] args)
         {
+            Dictionary dictonary = new Dictionary();
 
+            dictonary.CreateNewDictionary("English_Russian");
+            dictonary.AddWord("English_Russian", "Hello", "Привет");
+            dictonary.AddWord("English_Russian", "Word", "Слово");
         }
     }
+
+    
 }
